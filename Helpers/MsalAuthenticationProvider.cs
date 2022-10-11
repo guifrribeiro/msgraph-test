33fa0177acd191739edb52ed2a73a2b328cfd777
Guilherme Ferreira <guifrribeiro@gmail.com>
2022-10-11 15:51:14 -0300

    MSAL authentication provider

---
+++ Helpers/MsalAuthenticationProvider.cs
@@ -0,0 +1,32 @@
+using System.Net.Http.Headers;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace Helpers
+{
+  public class MsalAuthenticationProvider : IAuthenticationProvider
+  {
+    private IConfidentialClientApplication _clientApplication;
+    private string[] _scopes;
+
+    public MsalAuthenticationProvider(IConfidentialClientApplication clientApplication, string[] scopes)
+    {
+      _clientApplication = clientApplication;
+      _scopes = scopes;
+    }
+
+    public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+    {
+      var token = await GetTokenAsync();
+      request.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+    }
+
+    private async Task<string> GetTokenAsync()
+    {
+      AuthenticationResult authResult;
+      authResult = await _clientApplication.AcquireTokenForClient(_scopes).ExecuteAsync();
+
+      return authResult.AccessToken;
+    }
+  }
+}
